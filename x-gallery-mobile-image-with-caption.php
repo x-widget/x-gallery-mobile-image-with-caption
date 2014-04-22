109d87cc021d9334277d9d6b3beac47bc2cac570
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-04-22 17:18:52 +0800

    finished widget

---
+++ x-gallery-mobile-image-with-caption.php
@@ -0,0 +1,73 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+include_once(G5_LIB_PATH.'/thumbnail.lib.php');
+
+widget_css();
+
+if( $widget_config['forum1'] ) $_bo_table = $widget_config['forum1'];
+else $_bo_table = $widget_config['default_forum_id'];
+
+if ( empty($_bo_table) ) jsAlert('Error: empty $_bo_table ? on widget :' . $widget_config['name']);
+
+if( $widget_config['no'] ) $limit = $widget_config['no'];
+else $limit = 2;
+
+$list = g::posts( array(
+			"bo_table" 	=>	$_bo_table,
+			"limit"		=>	$limit,
+			"select"	=>	"idx,domain,bo_table,wr_id,wr_parent,wr_is_comment,wr_comment,ca_name,wr_datetime,wr_hit,wr_good,wr_nogood,wr_name,mb_id,wr_subject,wr_content"
+				)
+		);
+		
+?>
+<div class='gallery_mobile_images_with_captions'>
+<?php
+	if ( $list ) {	
+		foreach( $list as $li ) {			
+?>
+				<div class='container'>
+					<div class='images_with_captions'>
+						<div class='caption_image'>					
+						<?						
+							$imgsrc = get_list_thumbnail($_bo_table, $li['wr_id'], 378, 128);							
+							if ( empty($imgsrc['src']) )  $imgsrc['src'] = x::url().'/widget/'.$widget_config['name'].'/img/no-image.png';
+														
+							$img = "<img src='$imgsrc[src]'/>";						
+							echo "<div class='img-wrapper'><a href='$li[url]'>".$img."</a></div>";
+							
+							$post_subject = $li['subject'];
+														
+							$post_content = cut_str( strip_tags($li['wr_content']),"300","..." );							
+						?>
+						</div>
+						<div class='caption'>
+							<div class='subject'><a href='<?=$li['url']?>'><?=$post_subject?></a></div>
+							<div class='content'><a href='<?=$li['url']?>'><?=$post_content?></a></div>
+						</div>	
+					</div>					
+				</div>		
+	<?
+		}
+	}
+	else {
+		for ( $i = 0; $i < 2; $i++ ) {?>
+			<div class='container <?=$nomargin?>'>
+				<div class='images_with_captions'>
+						<div class='caption_image'>					
+						<? $imgsrc['src'] = $latest_skin_url.'/img/no-image.png';
+														
+							$img = "<img src='$imgsrc[src]'/>";						
+							echo "<div class='img-wrapper'><a href='".url_site_config()."'>".$img."</a></div>";
+						?>
+						</div>
+					<div class='caption'>
+						<div class='subject'><a href='<?=url_site_config()?>'>글을 등록해 주세요</a></div>
+					</div>						
+				</div>
+			</div>		
+	<?
+		}
+	}	
+?>
+	<div style='clear:both'></div>
+</div>
